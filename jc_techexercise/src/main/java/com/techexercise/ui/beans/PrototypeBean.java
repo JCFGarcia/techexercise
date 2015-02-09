5139d008b564ac1733685ce86d1f4eda03058074
JCFGarcia <robored61389@hotmail.com>
2015-02-08 22:33:22 -0600

    Initial local checkin

---
+++ jc_techexercise/src/main/java/com/techexercise/ui/beans/PrototypeBean.java
@@ -0,0 +1,20 @@
+package com.techexercise.ui.beans;
+
+import java.io.Serializable;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+@ManagedBean
+@ViewScoped
+public class PrototypeBean{
+
+    private String testString = "Hello World!";
+ 
+    public String getTestString() {
+        return testString;
+    }
+
+    public void setTestString(String testString) {
+        this.testString = testString;
+    }
+}
---
+++ jc_techexercise/src/main/webapp/resources/js/common.js
@@ -0,0 +1,49 @@
+Techexercise = {
+    utils : {
+        returnValue : function(formId, elemId, value) {
+            var element = window.opener.document.forms[formId].elements[elemId];
+            if (element != null) {
+                element.value = value;
+                element.focus();
+            }
+
+            window.close();
+        },
+        scrollToPageBottom : function() {
+            Solutionary.utils.scrollTo('footerDiv');
+        },
+        scrollTo : function(elementId) {
+            document.getElementById(elementId).scrollIntoView(false);
+        },
+        setTableRowEditable : function(row) {
+            jQuery('.ui-icon-pencil')[row].id = 'addedRow' + row;
+              jQuery('#addedRow' + row).trigger('click');
+           }
+    }
+};
+
+PrimeFaces.widget.Dialog.prototype.applyFocus = function() {
+    var firstInput = this.jq
+            .find(':not(:submit):not(:button):input:visible:enabled:first');
+    if (!firstInput.hasClass('hasDatepicker')) {
+        firstInput.focus();
+    }
+};
+
+$(function() {
+    if (document.getElementById('pageBody')) {
+        var viewportHeight;
+        if (document.compatMode === 'BackCompat') {
+            viewportHeight = document.body.clientHeight;
+        } else {
+            viewportHeight = document.documentElement.clientHeight;
+        }
+        document.getElementById('pageBody').style.minHeight = viewportHeight - 150 + 'px';
+    }
+});
+
+function handlePFDialogOnComplete(xhr, status, args, dialogWidgetVar) {
+    if (!args.validationFailed) {
+        PF(dialogWidgetVar).hide();
+    }
+}
